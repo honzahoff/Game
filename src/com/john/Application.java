47b623f108b2be2388646685804a64e946a861e0
Exiro7 <honzahoff@gmail.com>
2018-12-07 09:16:24 +0100

    1.0

---
+++ src/com/john/Application.java
@@ -0,0 +1,27 @@
+package com.john;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Application extends JFrame {
+
+    public Application(){
+        initUI();
+    }
+
+    public void initUI(){
+
+        add(new Board());
+        setSize(960,540);
+        setTitle("Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()-> {
+            Application ex = new Application();
+            ex.setVisible(true);
+        });
+
+    }
+}
---
+++ src/com/john/Board.java
@@ -0,0 +1,4 @@
+package com.john;
+
+public class Board {
+}
